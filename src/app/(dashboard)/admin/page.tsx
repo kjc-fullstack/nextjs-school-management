ed471766236e37c24a080e70228a1748f9f18128
kjc-fullstack <kjc.fullstack@gmail.com>
2025-10-16 15:52:55 +0100

    Design dashboard top cards (responsive)

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,6 +1,25 @@
+import UserCard from "@/components/UserCard"
+
 const AdminPage = () => {
   return (
-    <div className=''>AdminPage</div>
+    <div className='p-4 flex gap-4 flex-col md:flex-row'>
+      {/* LEFT SIDE */}
+      <div className='w-full lg:w-2/3'>
+        {/* USER CARDS */}
+        <div className="flex gap-4 justify-between flex-wrap">
+          <UserCard type='student' />
+          <UserCard type='teacher' />
+          <UserCard type='parent' />
+          <UserCard type='staff' />
+        </div>
+      </div>
+
+      {/* RIGHT SIDE */}
+      <div className='w-full lg:w-1/3'>
+        RIGHT
+      </div>
+
+    </div>
   )
 }
 
---
+++ src/components/UserCard.tsx
@@ -0,0 +1,17 @@
+import Image from "next/image"
+
+const UserCard = ({ type }: { type: string }) => {
+    return (
+        <div className="rounded-2xl odd:bg-kjcPurple even:bg-kjcYellow p-4 flex-1 min-w-[130px]">
+            <div className="flex justify-between items-center">
+                <span className="text-[10px] bg-white px-2  py-1 text-green-600 rounded-full">2024/25</span> 
+                <Image src="/more.png" alt="More" width={20} height={20} />
+            </div>
+            <h1 className="text-2xl font-semibold my-4">250</h1>
+            <h2 className="capitalize text-sm font-medium text-gray-500">{type}</h2>
+        
+        </div>
+    )
+}
+
+export default UserCard
