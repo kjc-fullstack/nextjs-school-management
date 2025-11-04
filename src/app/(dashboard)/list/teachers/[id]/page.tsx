1a580dd1441b659b130d9fd8c9b6e40f0fa42bf6
kjc-fullstack <kjc.fullstack@gmail.com>
2025-11-04 17:29:15 +0100

    Design single teacher page layout

--- src/app/(dashboard)/list/teachers/[id]/page.tsx
+++ src/app/(dashboard)/list/teachers/[id]/page.tsx
@@ -1,8 +1,12 @@
+import Announcements from "@/components/Announcements"
+import BigCalendar from "@/components/BigCalendar"
+import PerformanceChart from "@/components/PerformanceChart"
 import Image from "next/image"
+import Link from "next/link"
 
 const SingleTeacherPage = () => {
     return (
-        <div className="flex-1 p-4 flex-col gap-4 xl:flex-row">
+        <div className="flex-1 p-4 flex flex-col gap-4 xl:flex-row">
             {/* LEFT SIDE */}
             <div className="w-full xl:w-2/3">
                 {/* TOP SECTION */}
@@ -10,14 +14,14 @@ const SingleTeacherPage = () => {
                     {/* USER INFO CARD */}
                     <div className="bg-kjcSky py-6 px-4 rounded-md flex-1 flex gap-4">
 
-                        <div className="w-1/3">
+                        <div className="w-[23%]">
                             <Image src="https://images.pexels.com/photos/1516680/pexels-photo-1516680.jpeg?auto=compress&cs=tinysrgb&w=1200" 
                             alt="Teacher's photo" 
                             width={144} 
                             height={144} 
                             className="w-36 h-36 rounded-full object-cover"/>
                         </div>
-                        <div className="w-2/3 flex flex-col justify-between gap-4">
+                        <div className="w-[77%] flex flex-col justify-between gap-4">
                             <h1 className="text-xl font-semibold">Verna Marsh</h1>
                             <p className="text-sm text-gray-500"> Lorem, ipsum dolor sit amet consectetur adipisicing elit.
                             </p>
@@ -29,7 +33,7 @@ const SingleTeacherPage = () => {
 
                                 <div className="w-full md:w-1/3 lg:w-full 2xl:w-1/3 flex items-center gap-2">
                                     <Image src="/date.png" alt="" width={14} height={14}/>
-                                    <span>Jan 2025</span>
+                                    <span>Janvier 2025</span>
                                 </div>
 
                                 <div className="w-full md:w-1/3 lg:w-full 2xl:w-1/3 flex items-center gap-2">
@@ -39,9 +43,8 @@ const SingleTeacherPage = () => {
 
                                 <div className="w-full md:w-1/3 lg:w-full 2xl:w-1/3 flex items-center gap-2">
                                     <Image src="/phone.png" alt="" width={14} height={14}/>
-                                    <span>253 548 7</span>
+                                    <span className=""> 651 54 87 18</span>
                                 </div>
-                                
                             </div>
 
                         </div>
@@ -49,19 +52,80 @@ const SingleTeacherPage = () => {
                     </div>
 
                     {/* SMALL CARDS */}
-                    <div className="flex-1">
+                    <div className="flex-1 flex gap-4 justify-between flex-wrap">
+                        {/* CARD */}
+                        <div className="bg-white p-4 rounded-md flex gap-4 w-full md:w-[48%] xl:w-[45%] 2xl:w-[48%]">
+                            <Image src="/singleAttendance.png" alt="" width={24} height={24} className="w-6 h-6" />
+
+                            <div className="">
+                                <h1 className="text-xl font-semibold">90%</h1>
+                                <span className="text-sm text-gray-400">Attendance</span>
+                            </div>
+                        </div>
+
+                        {/* CARD */}
+                        <div className="bg-white p-4 rounded-md flex gap-4 w-full md:w-[48%] xl:w-[45%] 2xl:w-[48%">
+                            <Image src="/singleBranch.png" alt="" width={24} height={24} className="w-6 h-6" />
+
+                            <div className="">
+                                <h1 className="text-xl font-semibold">2</h1>
+                                <span className="text-sm text-gray-400">Branches</span>
+                            </div>
+                        </div>
+
+                        {/* CARD */}
+                        <div className="bg-white p-4 rounded-md flex gap-4 w-full md:w-[48%] xl:w-[45%] 2xl:w-[48%">
+                            <Image src="/singleLesson.png" alt="" width={24} height={24} className="w-6 h-6" />
+
+                            <div className="">
+                                <h1 className="text-xl font-semibold">6</h1>
+                                <span className="text-sm text-gray-400">Lessons</span>
+                            </div>
+                        </div>
+
+                        {/* CARD */}
+                        <div className="bg-white p-4 rounded-md flex gap-4 w-full md:w-[48%] xl:w-[45%] 2xl:w-[48%">
+                            <Image src="/singleClass.png" alt="" width={24} height={24} className="w-6 h-6" />
+
+                            <div className="">
+                                <h1 className="text-xl font-semibold">4</h1>
+                                <span className="text-sm text-gray-400">Classes</span>
+                            </div>
+                        </div>
 
                     </div>
                 </div>
 
                 {/* BOTTOM SECTION */}
-                <div className="">
-                    SCHEDULE
+                <div className="mt-4 bg-white rounded-md p-4 h-[800px]">
+                    <h1>Teacher&apos;s Schedule</h1>   
+                    <BigCalendar /> 
                 </div>
             </div>
             {/* RIGHT SIDE */}
-            <div className="w-full xl:w-1/3">
-                RIGHT
+            <div className="w-full xl:w-1/3 flex flex-col gap-4">
+                <div className="bg-white p-4 rounded-md">
+                    <h1 className="text-xl font-semibold">Shortcuts</h1>
+                </div>
+                <div className="mt-4 flex gap-4 flex-wrap text-xs text-gray-500">
+                        <Link className="p-3 rounded-md bg-kjcSkyLight" href="/">
+                            Teacher&apos;s Classes
+                        </Link>
+                        <Link className="p-3 rounded-md bg-kjcPurpleLight" href="/">
+                            Teacher&apos;s Students
+                        </Link>
+                        <Link className="p-3 rounded-md bg-kjcYellowLight" href="/">
+                            Teacher&apos;s Lessons
+                        </Link>
+                        <Link className="p-3 rounded-md bg-pink-50" href="/">
+                            Teacher&apos;s Exams
+                        </Link>
+                        <Link className="p-3 rounded-md bg-kjcSkyLight" href="/">
+                            Teacher&apos;s Assignments
+                        </Link>
+                </div>
+                <PerformanceChart />
+                <Announcements />
             </div>
         </div>
     )
